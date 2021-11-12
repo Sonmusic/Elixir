0f1e7614975625c4df10046d2231855147a55222
Sonmusic <omar.m.sanchez@hotmail.com>
2021-11-12 00:23:48 -0400

    My first Commit

---
+++ lib/try.ex
@@ -0,0 +1,24 @@
+defmodule Try do
+  def commandlinearg(argv) do
+    IO.puts("Hola #{argv}")
+  end
+
+
+  def standard do
+    IO.puts("Introduce una palabra para standard output")
+    word = IO.gets("")
+    IO.puts("standard output: #{word}")
+    IO.puts(:stderr, "standard error: error!")
+  end
+
+  def readingafile do
+    file = "hello.txt"
+    File.read(file)
+  end
+
+  def updatingafile do
+    file = "hello.txt"
+    File.write(file, "Actualizado!")
+    File.read(file)
+  end
+end
